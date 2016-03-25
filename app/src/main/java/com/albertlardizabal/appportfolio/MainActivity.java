bd5190dfb5d9343a19ef926a80ee437974fc6a51
alardizabal <albie06@gmail.com>
2016-03-25 01:35:19 -0400

    added more styling. ready for initial submission

--- app/src/main/java/com/albertlardizabal/appportfolio/MainActivity.java
+++ app/src/main/java/com/albertlardizabal/appportfolio/MainActivity.java
@@ -1,7 +1,13 @@
 package com.albertlardizabal.appportfolio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void didTapAppButton(View view) {
+        Button button = (Button)view;
+        String buttonText = button.getText().toString();
+        showToast(buttonText);
+    }
+
+    public void showToast(String string) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch " + string + "!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        if (v != null) v.setGravity(Gravity.CENTER);
+        toast.show();
+    }
+
 }
